6bc354337e72f1dfd90062462e778fe096656e93
sascha <s.lumma@gmail.com>
2025-06-22 20:53:38 +0200

    Paying with basic rust constructs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::Rng;
+use std::io;
+
+fn read_line() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+}
+
+fn generate_number() -> i32 {
+    rand::rng().random_range(1..=100)
+}
+
+const TOP: char = '\u{2500}';
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input: {TOP} {} {}", generate_number(), read_line());
 }
